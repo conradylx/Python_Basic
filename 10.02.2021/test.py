8c79337c7a4743163d5305ce26d5fbc9fa318a33
conradylx <50596942+conradylx@users.noreply.github.com>
2021-02-10 20:21:17 +0100

    Lection_01

---
+++ 10.02.2021/test.py
@@ -0,0 +1,12 @@
+# print("*")
+# print("**")
+# print("***")
+# print(" ")
+# for i in range(1,4):
+#     print("*"*i)
+
+mass = int( input('Podaj swoją wagę w kg:') )
+height = float(input('Podaj swój wzrost:'))
+BMI = wagmassa / (height ** 2)
+BMI_rounded = round(BMI, 2)
+print('Moje BMI ', BMI_rounded)
---
+++ 10.02.2021/weeks.py
@@ -0,0 +1,6 @@
+mins = 60*24*7*7
+weeks = (75/8)//7
+
+print(mins)
+print()
+print(weeks)
