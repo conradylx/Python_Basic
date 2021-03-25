03e0820733bba0cc8ea2be0d96cdb6351673e0fe
conradylx <50596942+conradylx@users.noreply.github.com>
2021-03-25 13:16:55 +0100

    Folder structure modification

--- 17.02.2021/Zadania/ex1.py
+++
@@ -1,9 +0,0 @@
-# Oblicz koszt wyprawy znając dystans, spalanie na 100km i cenę litra benzyny.
-# Załóżmy, że spalanie na 100km wynosi 6,4 l, trasa to 120km, litr benzyny kosztuje 5,04 zł.
-
-distance = float(input("Enter distance "))
-burning = float(input("Enter burning "))
-price = float(input("Enter price "))
-
-result = (distance * burning) / 100 * price
-print(result)
--- 17.02.2021/Zadania/ex7.py
+++
@@ -1,5 +0,0 @@
-# Przekopiuj zawartość import this do zmiennej.
-import this
-
-text = this
-print(text)
