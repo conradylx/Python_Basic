fdc89043bcb9b6eb9394b7402e24dc8ae5e16a9a
conradylx <50596942+conradylx@users.noreply.github.com>
2021-04-18 14:08:41 +0200

    Fixed excercise 2

--- 14.04.2021/exc2/exc2.py
+++ 14.04.2021/exc2/exc2.py
@@ -1,25 +1,18 @@
 # Napisz kod, który zwraca sumę wszystkich wielokrotności 5 lub 7 poniżej liczby N.
 
-def multiply_by_5_or_7(number, result, multi, sum):
-    sum += result
-
-    if result < number:
-        return multiply_by_5_or_7(number, result * multi, multi, sum)
-    elif result == number:
-        return sum
+def addition_by_number(number, add_by_num, temp_sum, sum_of_nums):
+    if temp_sum < number:
+        return addition_by_number(number, add_by_num, temp_sum+add_by_num, sum_of_nums+temp_sum)
     else:
-        sum -= result
-        return sum
+        return sum_of_nums
 
 
 def return_multi_by_5_or_7(number):
-    sum = 0
-    choice = int(input("Do you want sum multiply of 5 or 7? Enter 5 or 7\n"))
-    while choice != 5 and choice != 7:
-        choice = int(input(input("Enter 5 or 7: \n")))
-    multi = choice
-    result = multiply_by_5_or_7(number, choice, multi, sum)
-    return result
+    addition_number = 5
+    sum_of_5s = addition_by_number(number, addition_number, 0, 0)
+    addition_number = 7
+    sum_of_7s = addition_by_number(number, addition_number, 0, 0)
+    return sum_of_5s + sum_of_7s
 
 
 if __name__ == '__main__':
