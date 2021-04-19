f95bec6b6e187062a27abf910b47bd7f04b70fbd
conradylx <50596942+conradylx@users.noreply.github.com>
2021-04-19 18:44:48 +0200

    Pascal triangle - short version'

--- 14.04.2021/exc6/exc6.py
+++ 14.04.2021/exc6/exc6.py
@@ -1,20 +1,12 @@
 # Poznaj trójkąt Pascala. Napisz kod, który wyświetla trójkąt Pascala dla podanego N.
 
-def find_factorial(i):
-    if i == 1 or i == 0:
-        return 1
-    else:
-        return i * find_factorial(i - 1)
-
-
-def find_rows(n, k):
-    return int(find_factorial(n)/(find_factorial(n-k)*find_factorial(k)))
-
 
 def pascal_triangle(rows):
-    for n in range(0, rows):
-        for k in range(0, n + 1):
-            print(find_rows(n, k), " ", end="")
+    for line in range(1, rows + 1):
+        temp = 1
+        for number in range(1, line + 1):
+            print(temp, ' ', end='')
+            temp = int(temp * (line - number) / number)
         print()
 
 
